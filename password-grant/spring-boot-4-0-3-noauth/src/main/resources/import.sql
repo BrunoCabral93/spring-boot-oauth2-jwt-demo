263eb5a140b48a1b8b8e77a934837e66064b4b7e
Bruno Cabral <bruno.cabral93@hotmail.com>
2026-05-12 22:44:36 -0300

    BCrypt password

--- password-grant/spring-boot-4-0-3-noauth/src/main/resources/import.sql
+++ password-grant/spring-boot-4-0-3-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$MdMywFSW4M13cSiL0l6.sePgyvFVCL0jQ0ebtc/p5ZuCgJ/Q6KSrK');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$MdMywFSW4M13cSiL0l6.sePgyvFVCL0jQ0ebtc/p5ZuCgJ/Q6KSrK');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
