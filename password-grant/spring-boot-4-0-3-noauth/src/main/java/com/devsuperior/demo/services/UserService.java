df99ba0e57be1249029a4eb6edbcbee437b83d62
Bruno Cabral <bruno.cabral93@hotmail.com>
2026-05-28 20:11:25 -0300

    Access control by role, by route

--- password-grant/spring-boot-4-0-3-noauth/src/main/java/com/devsuperior/demo/services/UserService.java
+++ password-grant/spring-boot-4-0-3-noauth/src/main/java/com/devsuperior/demo/services/UserService.java
@@ -8,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UserService implements UserDetailsService {
 
     @Autowired
@@ -24,7 +26,7 @@ public class UserService implements UserDetailsService {
         throw new UsernameNotFoundException("User not found");
     }
     User user = new User();
-    user.setEmail(username);
+    user.setEmail(result.get(0).getUsername());
     user.setPassword(result.get(0).getPassword());
     for (UserDetailsProjection projection : result){
         user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
